0950059a3afb4d96c15e294a56db39903b749ccf
docfate111 <tdwilliamsiv@gmail.com>
2021-05-24 23:56:42 -0500

    debugging character device

--- device_drivers/kernel/so2_cdev.c
+++ device_drivers/kernel/so2_cdev.c
@@ -19,7 +19,7 @@ MODULE_DESCRIPTION("SO2 character device");
 MODULE_AUTHOR("SO2");
 MODULE_LICENSE("GPL");
 
-#define LOG_LEVEL	KERN_INFO
+#define LOG_LEVEL 	KERN_INFO
 
 #define MY_MAJOR		42
 #define MY_MINOR		0
@@ -49,17 +49,20 @@ static int so2_cdev_open(struct inode *inode, struct file *file)
 {
 	struct so2_device_data *data;
 	/* TODO 2: print message when the device file is open. */
-	printk("Device file opened");
+	printk( LOG_LEVEL "Device file opened");
 	/* TODO 3: inode->i_cdev contains our cdev struct,
 	 use container_of to obtain a pointer to so2_device_data */
 	data = container_of(inode->i_cdev, struct so2_device_data, cdev);
 	file->private_data = data;
 	/* TODO 3: return immediately if access is != 0,
 	use atomic_cmpxchg */
-	int old = atomic_cmpxchg(&(data->accessible), 0, 1);
-	if(old!=1){ return -1; }
+	if( atomic_cmpxchg(&(data->accessible), 0, 1) != 1 ){
+		printk( LOG_LEVEL "target was busy sorry");
+		return -EBUSY;
+	} // device is busy
 	set_current_state(TASK_INTERRUPTIBLE);
 	schedule_timeout(10 * HZ);
+	printk( LOG_LEVEL "opened the device");
 	return 0;
 }
 
@@ -67,14 +70,10 @@ static int
 so2_cdev_release(struct inode *inode, struct file *file)
 {
 	/* TODO 2: print message when the device file is closed. */
-	printk("Device file is closed");
-#ifndef EXTRA
-	struct so2_device_data *data =
-		(struct so2_device_data *) file->private_data;
-
+	printk( LOG_LEVEL "Device file is closed");
+	struct so2_device_data *data = (struct so2_device_data *) file->private_data;
 	/* TODO 3: reset access variable to 0, use atomic_set */
 	atomic_set(&(data->accessible), 0);
-#endif
 	return 0;
 }
 
@@ -85,14 +84,14 @@ so2_cdev_read(struct file *file,
 {
 	struct so2_device_data *data =
 		(struct so2_device_data *) file->private_data;
-	size_t to_read = min(size, size - *offset);
+	size_t to_read = size - *offset;
 	if(to_read <= 0){ return 0; }
 #ifdef EXTRA
 	/* TODO 7: extra tasks for home */
 #endif
 	/* TODO 4: Copy data->buffer to user_buffer */
 	if(size <= BUFSIZ){
-		printk("being read");
+		printk( LOG_LEVEL "being read");
 		to_read = (size_t)copy_to_user(user_buffer,
 				data->buffer + *offset, to_read);
 		if(to_read){ return -EFAULT; }
@@ -124,18 +123,29 @@ unsigned long arg)
 	struct so2_device_data *data =
 		(struct so2_device_data *) file->private_data;
 	int ret = 0;
-
 	switch (cmd) {
 		/* TODO 6: display IOCTL_MESSAGE */
 		case MY_IOCTL_PRINT:
-			if(arg){
-				copy_to_user(arg, IOCTL_MESSAGE, 11);
-			} else {
-				printk("Ioctl message is %s", IOCTL_MESSAGE);
-			}
+			printk( LOG_LEVEL "Ioctl message is %s", IOCTL_MESSAGE);
 		/* TODO 7: extra tasks, for home */
+			break;
+		case MY_IOCTL_SET_BUFFER:
+			if( copy_from_user(data->buffer, (struct so2_device_data *)arg, BUFSIZ) )
+            			return -EFAULT;
+			break;
+		case MY_IOCTL_GET_BUFFER:
+			if( copy_to_user((struct so2_device_data *)arg, data->buffer, BUFSIZ) )
+				return -EFAULT;
+			break;
+		case MY_IOCTL_DOWN:
+			
+			break;
+		case MY_IOCTL_UP:
+			
+			break;
 		default:
 			ret = -EINVAL;
+			break;
 	}
 	return ret;
 }
@@ -157,13 +167,11 @@ static int so2_cdev_init(void)
 {
 	int err;
 	int i;
-	printk("Registering the char device");
+	printk( LOG_LEVEL "Registering the char device");
 	/* TODO 1: register char device region for MY_MAJOR and NUM_MINORS starting at MY_MINOR */
 	register_chrdev_region(MKDEV(MY_MAJOR, MY_MINOR), NUM_MINORS, MODULE_NAME);
 	for (i = 0; i < NUM_MINORS; i++) {
-#ifdef EXTRA
 		/* TODO 7: extra tasks, for home */
-#else
 		/*TODO 4: initialize buffer with MESSAGE string */
 		strncpy(devs[i].buffer, MESSAGE, BUFSIZ);
 		/* TODO 7: extra tasks for home */
@@ -172,7 +180,6 @@ static int so2_cdev_init(void)
 		/* TODO 2: init and add cdev to kernel core */
 		cdev_init(&devs[i].cdev, &so2_fops);
 		cdev_add(&devs[i].cdev, MKDEV(MY_MAJOR, i), 1);
-#endif
 	}
 	return 0;
 }
@@ -184,7 +191,7 @@ static void so2_cdev_exit(void)
 		/* TODO 2: delete cdev from kernel core */
 		cdev_del(&devs[i].cdev);
 	}
-	printk("Unregistering char device");
+	printk( LOG_LEVEL "Unregistering char device");
 	/* TODO 1: unregister char device region, for MY_MAJOR and NUM_MINORS starting at MY_MINOR */
 	unregister_chrdev_region(MKDEV(MY_MAJOR, MY_MINOR), NUM_MINORS);
 }
